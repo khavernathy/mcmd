b3b9b5adfafd320cb6baa94e6eadda0008dffcca
Douglas M Franz <dfranz@mail.usf.edu>
2017-04-26 22:28:41 -0400

    edited the commy potential to include LJ repulsion and scaled-down attraction

--- src/commy.cpp
+++ src/commy.cpp
@@ -13,34 +13,51 @@ double commy(System &system) {
     double cutoff = system.pbc.cutoff;
     double volume = system.pbc.volume;
     int i,j,k,l;
-    double r,r7;
+    double r,sr6,r7;
     double numerator= system.constants.HBARC * -23.0;
     double FourPi = 12.566370614359172;
     double polar1, polar2;    
+    double attractive, repulsive; // energies
 
     for (i = 0; i < system.molecules.size(); i++) {
     for (j = 0; j < system.molecules[i].atoms.size(); j++) {
     for (k = i+1; k < system.molecules.size(); k++) { // so if one frozen molecule, frozen-frozen is ignored.
     for (l =0; l < system.molecules[k].atoms.size(); l++) {
 
+        attractive=0; repulsive=0;
+
+        // do mixing rules
+        double eps = system.molecules[i].atoms[j].eps,sig=system.molecules[i].atoms[j].sig;
+        if (eps != system.molecules[k].atoms[l].eps)
+            eps = sqrt(eps * system.molecules[k].atoms[l].eps);
+        if (sig != system.molecules[k].atoms[l].sig)
+            sig = 0.5 * (sig + system.molecules[k].atoms[l].sig);
+
         polar1 = system.molecules[i].atoms[j].polar;
         polar2 = system.molecules[k].atoms[l].polar;
-
-        if (polar1 == 0. || polar2 == 0.) continue; // skip 0-energy
 
         // calculate distance between atoms
         double* distances = getDistanceXYZ(system, i, j, k, l);
         r = distances[3]; //printf("r %f\n", r);
-        r7= r*r; // r2
-        r7 *= r7 * r7; // r6
-        r7 *= r; // r7
+        if (sig != 0 && eps != 0) {
+            sr6 = sig/r;
+            sr6 *= sr6;
+            sr6 *= sr6*sr6;
+            repulsive = 4.0*eps*(sr6*sr6);
+        }
 
-    
+        if (polar1 != 0 && polar2 != 0) {
+            r7= r*r; // r2
+            r7 *= r7 * r7; // r6
+            r7 *= r; // r7
+            attractive = ((numerator/(FourPi*r7)) * polar1 * polar2) / 50; // I'm doing the division here to scale down
+        }
 
         if ((r <= cutoff)) {
-    
             //printf("num %f denom %f polar1 %f polar2 %f\n", numerator, FourPi*r7, system.molecules[i].atoms[j].polar, system.molecules[k].atoms[l].polar);
-            total_pot += (numerator/(FourPi*r7)) * polar1 * polar2;
+            // communist potential is attractive contribution; using normal LJ repulsion contribution
+            total_pot += attractive + repulsive;
+            //printf("attractive: %f repulsive: %f\n", attractive, repulsive);
         }
         
     }  // loop l
