1fb52f2714e89515a40a4c4034e7ec2fed08bc1a
khavernathy <dfranz@mail.usf.edu>
2017-04-30 14:15:08 -0400

    readme update

--- src/main.cpp
+++ src/main.cpp
@@ -77,7 +77,7 @@ int main(int argc, char **argv) {
     setupFugacity(system);
 
     initialize(system); // these are just system name sets,
-    printf("VERSION NUMBER: %i\n", 288); // i.e. github commit
+    printf("VERSION NUMBER: %i\n", 336); // i.e. github commit
     system.checkpoint("Done with system setup functions.");
 
     printf("SORBATE COUNT: %i\n", (int)system.proto.size());
