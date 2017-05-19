e15fc005e0ed7fbd30fd6918b52c3aa1df4ef986
khavernathy <dfranz@mail.usf.edu>
2017-05-18 23:18:47 -0400

    fugacity bias set to 3x for bias-N

--- src/boltzmann.cpp
+++ src/boltzmann.cpp
@@ -22,7 +22,7 @@ double get_boltzmann_factor(System &system, double e_i, double e_f, int_fast8_t 
     // check for N bias (loading bias)
     if (system.constants.bias_uptake_switcher) {
         if ((double)system.stats.count_movables < system.constants.bias_uptake)
-            fugacity = 1e4; // big, but not too big, for biased insertions
+            fugacity *= 3; // big, but not too big, for biased insertions
         else {
             system.constants.bias_uptake_switcher=0; // done biasing if we exceed the bias-N
             printf("Loading bias deactivated! (Desired N reached).\n");
