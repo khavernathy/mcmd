7b187033e3c7c5e7eb11f86696db3cfe1f472280
Douglas M Franz <dfranz@mail.usf.edu>
2017-11-25 15:29:53 -0500

    format quadrupole

--- src/sp.cpp
+++ src/sp.cpp
@@ -60,12 +60,17 @@ void singlePointEnergy(System &system) {
     for (n=0; n<6; n++)
         multipole2[n] *= system.constants.eA2D;
 
-    printf("Multipole 0 (total charge)      = { %5.5f } e\n", multipole0);
-    printf("Multipole 1 (dipole moment)     = { %5.5f %5.5f %5.5f } Debye\n", multipole1[0], multipole1[1], multipole1[2]);
-    printf("            (dipole magnitude)  = { %5.5f } Debye\n", dipole_magnitude);
-    printf("Multipole 2 (quadrupole moment) = { %5.5f %5.5f %5.5f } Debye A\n", multipole2[0], multipole2[3], multipole2[4]);
-    printf("                                  { %5.5f %5.5f %5.5f } Debye A\n", multipole2[3], multipole2[1], multipole2[5]);
-    printf("                                  { %5.5f %5.5f %5.5f } Debye A\n", multipole2[4], multipole2[5], multipole2[2]);
+    printf("Multipole 0 (total charge)\n");
+    printf("{ %9.5f   } e\n\n", multipole0);
+
+    printf("Multipole 1 (dipole moment)\n");
+    printf("{ %9.5f %9.5f %9.5f   } Debye\n", multipole1[0], multipole1[1], multipole1[2]);
+    printf("(dipole magnitude)  = { %9.5f   } Debye\n\n", dipole_magnitude);
+    
+    printf("Multipole 2 (quadrupole moment)\n");
+    printf("{ %9.5f %9.5f %9.5f   } Debye A\n", multipole2[0], multipole2[3], multipole2[4]);
+    printf("{ %9.5f %9.5f %9.5f   } Debye A\n", multipole2[3], multipole2[1], multipole2[5]);
+    printf("{ %9.5f %9.5f %9.5f   } Debye A\n", multipole2[4], multipole2[5], multipole2[2]);
 
 
     return;
