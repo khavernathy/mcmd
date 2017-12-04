1eb986c3057a1e062c9b8d1d7f59574629212350
Douglas M Franz <dfranz@mail.usf.edu>
2017-12-03 22:31:10 -0500

    bonding con't. determine all bonds including duplicates on each atom. (If uniques are needed, simply loop i,j+1)

--- src/bonding.cpp
+++ src/bonding.cpp
@@ -1,8 +1,39 @@
+/* Douglas M Franz
+ * Space group, USF, 2017
+ * 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <cmath>
 
 using namespace std;
+
+/*
+ * Essentially everything below comes from the parameters and
+ * functional forms prescribed in UFF, via
+ * J. Am. Chem. Soc. Vol. 114, No. 25, 1992
+ * */
+
+// get the total potential from bond stretches
+// via the Morse potential
+double stretch_energy(System &system) {
+    return 0;
+}
+
+// get the total potential from angle bends
+// via simple Fourier small cosine expansion
+double angle_bend_energy(System &system) {
+    return 0;
+}
+
+// get the total potential from torsions
+// via simple Fourier small cosine expansion
+double torsions_energy(System &system) {
+    return 0;
+}
+
+
 
 
