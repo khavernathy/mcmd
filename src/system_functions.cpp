6bfab0109741f53a6bed6016e0d3a6c5731e5661
khavernathy <dfranz@mail.usf.edu>
2018-11-29 11:56:21 -0500

    do co2 fit if option is on, including case where single sorbate is used

--- src/system_functions.cpp
+++ src/system_functions.cpp
@@ -1030,20 +1030,23 @@ void setupFugacity(System &system) {
         else if (system.constants.fugacity_single_sorbate == "ch4") system.proto[0].fugacity = ch4_fugacity(system, system.constants.temp, system.constants.pres); 
         else if (system.constants.fugacity_single_sorbate == "off") system.proto[0].fugacity = system.constants.pres;     
     
-    } else if (system.constants.co2_fit_fugacity) {
-        printf("Using Laratelli CO2 fugacity fits.\n");
+    } else if (system.proto.size() == 1) {
+        // presumably this means we want to use use I.G. approx for the sorbate
+        system.proto[0].fugacity = system.constants.pres;
+    }
+    
+    // (over)write CO2 fugacity with Luci's fits if user desires
+    if (system.constants.co2_fit_fugacity) {
+        printf("Using Laratelli CO2 fugacity fits with pressure = %f atm.\n",system.constants.pres);
         for (int i=0; i<system.constants.sorbate_name.size();i++) {
             if (system.constants.sorbate_name[i] == "co2_phast*") {
                 system.proto[i].fugacity = phast_star(system.constants.pres);
             } else if (system.constants.sorbate_name[i] == "co2_trappe") {
                 system.proto[i].fugacity = trappe(system.constants.pres);
             }
-        }           
-    } else if (system.proto.size() == 1) {
-        // presumably this means we want to use use I.G. approx for the sorbate
-        system.proto[0].fugacity = system.constants.pres;
+        }
     }
-    
+
     for (int i=0; i<system.proto.size(); i++) {
         printf("Fugacity for prototype %i ( %s ) = %f atm\n", i, system.proto[i].name.c_str(), system.proto[i].fugacity);
     }
